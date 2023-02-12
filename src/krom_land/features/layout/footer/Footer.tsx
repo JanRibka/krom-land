35637cabc814bfe20125011ebf5fb8b24c3d0d4b
JanRibka <62721322+JanRibka@users.noreply.github.com>
2023-02-12 02:00:28 +0100

    Patička

--- src/krom_land/features/layout/footer/Footer.tsx
+++ src/krom_land/features/layout/footer/Footer.tsx
@@ -1,15 +1,27 @@
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import Stack from '@mui/material/Stack';
+import { useTheme } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
+import Copyright from './copyright/Copyright';
 import AboutUs from './img/about-us.webp';
 import Address from './img/address.webp';
 import Constct from './img/contact.webp';
 import InfoItem from './infoItem/InfoItem';
+import SocialIcons from './socialIcons/SocialIcons';
 import FooterStyled from './styledComponents/FooterStyled';
 
 const Footer = () => {
+  // Constants
+  const theme = useTheme();
+  const mdDwn: boolean = useMediaQuery(theme.breakpoints.down("md"));
+  const lgDwn: boolean = useMediaQuery(theme.breakpoints.down("lg"));
+  const xlDwn: boolean = useMediaQuery(theme.breakpoints.down("xl"));
+  const infoWrapperSpacing: number = mdDwn ? 5 : lgDwn ? 10 : xlDwn ? 15 : 20;
+  const infoWrapperDirection = mdDwn ? "column" : "row";
+
   // Other
   const RenderAboutUsContent = () => {
     return (
@@ -60,20 +72,29 @@ const Footer = () => {
 
   return (
     <FooterStyled component='footer'>
-      <Stack spacing={5} direction='column' className='footer-wrapper'>
-        <Stack direction='row' spacing={3}>
-          <InfoItem image={AboutUs} alt='O nás' description='O nás'>
-            {RenderAboutUsContent()}
-          </InfoItem>
-          <InfoItem image={Address} alt='Adresa' description='Adresa'>
-            {RenderAddressContent()}
-          </InfoItem>
-          <InfoItem image={Constct} alt='Kontakt' description='Kontakt'>
-            {RenderContactContent()}
-          </InfoItem>
-        </Stack>
-
-        <Divider className='divider' />
+      <Stack spacing={7} direction='column' className='footer-wrapper'>
+        <>
+          <Stack direction={infoWrapperDirection} spacing={infoWrapperSpacing}>
+            <InfoItem image={AboutUs} alt='O nás' description='O nás'>
+              {RenderAboutUsContent()}
+            </InfoItem>
+            <InfoItem image={Address} alt='Adresa' description='Adresa'>
+              {RenderAddressContent()}
+            </InfoItem>
+            <InfoItem image={Constct} alt='Kontakt' description='Kontakt'>
+              {RenderContactContent()}
+            </InfoItem>
+          </Stack>
+        </>
+        <>
+          <Divider className='divider' />
+        </>
+        <>
+          <SocialIcons />
+        </>
+        <>
+          <Copyright />
+        </>
       </Stack>
     </FooterStyled>
   );
