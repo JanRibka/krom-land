59ac3e673fdc52d43bbc0f0bce0dc3443963afb3
JanRibka <62721322+JanRibka@users.noreply.github.com>
2023-02-22 23:52:06 +0100

    Galerie

--- src/shared/components/picture/Picture.tsx
+++ src/shared/components/picture/Picture.tsx
@@ -1,18 +1,21 @@
-import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
-import Box from "@mui/material/Box";
+import PhotoModel from 'shared/models/PhotoModel';
 
-import PictureStyled from "./styledComponents/PistureStyled";
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+import Box from '@mui/material/Box';
+
+import PictureStyled from './styledComponents/PistureStyled';
 
 interface IProps {
-  image: string;
-  alt: string;
+  photo: PhotoModel;
   className?: string;
   enbHover?: boolean;
+  handleOpenGalleryOnClick?: (index: number | undefined) => void;
 }
 
 const Picture = (props: IProps) => {
   return (
     <PictureStyled
+      onClick={() => props.handleOpenGalleryOnClick?.(props.photo.Index)}
       className={
         "picture-wrapper" +
         (props.className || "") +
@@ -20,7 +23,12 @@ const Picture = (props: IProps) => {
       }
     >
       <Box className='image-overlay'></Box>
-      <Box component='img' src={props.image} alt={props.alt} loading='lazy' />
+      <Box
+        component='img'
+        src={props.photo.Src}
+        alt={props.photo.Alt}
+        loading='lazy'
+      />
       <VisibilityOutlinedIcon />
     </PictureStyled>
   );
