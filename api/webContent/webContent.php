a74ac64e8752fea17efea2dd2ca98acd372c83b1
JanRibka <62721322+JanRibka@users.noreply.github.com>
2023-03-10 00:25:47 +0100

    Načítání dat z db

--- api/webContent/webContent.php
+++ api/webContent/webContent.php
@@ -5,14 +5,37 @@ class WebContent
 {
   public static function getAll()
   {
-    $homeId = 0;
-    $result = new ResultModel();
+    $homeId = 1;
+    $actionsId = 1;
 
     try {
-      $homeQuery = dibi::query("SELECT * FROM home WHERE id = %i", $homeId);
+      $homeQuery = dibi::query(
+        "SELECT * FROM home as h WHERE h.id = %i",
+        $homeId
+      );
       $home = $homeQuery->fetch();
 
-      $result->Home = $home;
+      $actinsQuery = dibi::query(
+        "SELECT * FROM actions as a WHERE a.id = %i",
+        $actionsId
+      );
+      $actions = $actinsQuery->fetch();
+
+      $actinDeatilsQuery = dibi::query(
+        "SELECT ad.* FROM actions as a JOIN actionDeatil as ad on a.id = ad.ActionsId WHERE a.id = %i",
+        $actionsId
+      );
+      // TODO: Bude se tu t59dit detail akce podle poradi
+      $actions = new ActionsModel(
+        $actions->Id,
+        $actions->Title,
+        $actions->Description,
+        $actions->MainImagePath,
+        $actions->MainImageAlt,
+        $actinDeatilsQuery->fetchAll()
+      );
+
+      $result = new ResultModel($home, $actions);
 
       apiResponse(true, "", $result);
     } catch (Exception $ex) {
@@ -21,8 +44,40 @@ class WebContent
   }
 }
 
+class ActionsModel
+{
+  public $Id;
+  public $Title;
+  public $Description;
+  public $MainImagePath;
+  public $MainImageAlt;
+  public $ActionDetails;
+
+  public function __construct(
+    $id,
+    $title,
+    $description,
+    $mainImagePath,
+    $mainImageAlt,
+    $actionDetails
+  ) {
+    $this->Id = $id;
+    $this->Title = $title;
+    $this->Description = $description;
+    $this->MainImagePath = $mainImagePath;
+    $this->MainImageAlt = $mainImageAlt;
+    $this->ActionDetails = $actionDetails;
+  }
+}
 class ResultModel
 {
   public $Home;
+  public $Actions;
+
+  public function __construct($home, $actions)
+  {
+    $this->Home = $home;
+    $this->Actions = $actions;
+  }
 }
 ?>
